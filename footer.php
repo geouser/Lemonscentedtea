760a800029ee810d41e69ee229a94afc7ff5a6aa
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2018-04-23 18:50:26 +0300

    footer update

--- footer.php
+++ footer.php
@@ -1,41 +1,49 @@
         <div id="footer">
             <div class="container">
+
                 <div class="row">
-                    <div class="col-md-3 col-sm-2 col-xs-4 logo">
-                        <?php the_custom_logo(); ?>
-                    </div>
                     
-                    <div class="col-md-2 col-sm-4 col-xs-8 contact-info">
-                        <?php if(is_active_sidebar('footer-sidebar-1')){
-                                dynamic_sidebar('footer-sidebar-1');
-                            }
-                        ?>
+                    <div class="col-12 col-md-6">
+                        <div class="row">
+                            <div class="col-12 col-md-4 logo">
+                                <?php the_custom_logo(); ?>
+                            </div>
+                            <div class="col-12 col-md-8 contact-info ">
+                                <?php if(is_active_sidebar('footer-sidebar-1')){
+                                        dynamic_sidebar('footer-sidebar-1');
+                                    }
+                                ?>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-2"></div>
-                    
-                    <div class="col-md-2 col-sm-2 col-xs-4 footer-menu">
-                        <?php
-                            wp_nav_menu(array(
-                                'menu' => 'footer_menu',
-                                'menu_class' => 'menu footer-menu'
-                            ));
-                        ?>
+
+                    <div class="col-12 col-md-6">
+                        <div class="row justify-content-end-md">
+                            <div class="col-12 col-md-4 footer-menu text-left">
+                                <?php
+                                    wp_nav_menu(array(
+                                        'menu' => 'footer_menu',
+                                        'menu_class' => 'menu footer-menu'
+                                    ));
+                                ?>
+                            </div>
+                            <div class="col-12 col-md-4 subscribe text-left">
+                                <div class="title">Subcribe to our newsletter</div>
+                                <form id="form_subcribe">
+                                    <div><input id="email" placeholder="your e-mail"></div>
+                                    <div class="subcribe-submit"><input type="submit" value="Submit"></div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    
-                    <div class="col-md-3 col-sm-4 col-xs-8 subscribe">
-                        <div class="title">Subcribe to our newsletter</div>
-                        <form id="form_subcribe">
-                            <div><input id="email" placeholder="your e-mail"></div>
-                            <div class="subcribe-submit"><input type="submit" value="Submit"></div>
-                        </form>
-                        <!--<div class="disclaimer">Disclaimer</div>-->
-                    </div>
+
                 </div>
-            </div>
-        </div>
+
+            </div> <!-- end container -->
+        </div> <!-- end #footer -->
 
         <?php wp_footer(); ?>
-        
+
     </body>
 
 </html>
