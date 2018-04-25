ff42b1503fdca86cd75c8bb8c3fc3fddec4f6b66
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2018-04-25 15:23:47 +0300

    footer update

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
         <div id="footer">
             <div class="container-fluid">
 
-                <div class="row align-items-center">
+                <div class="row">
                     
-                    <div class="col-lg-6 col-md-7">
-                        <div class="row align-items-center">
+                    <div class="col-6">
+                        <div class="row">
                             <div class="col-auto logo">
                                 <?php the_custom_logo(); ?>
                             </div>
@@ -17,9 +17,9 @@
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-md-5">
-                        <div class="row justify-content-end align-items-center">
-                            <div class="col-md-auto footer-menu text-left">
+                    <div class="col-6">
+                        <div class="row justify-content-end justify-content-start-sm">
+                            <div class="col-auto footer-menu text-left">
                                 <?php
                                     wp_nav_menu(array(
                                         'menu' => 'footer_menu',
@@ -27,8 +27,8 @@
                                     ));
                                 ?>
                             </div>
-                            <div class="col-auto text-left">
-                                <form class="subscribe-form" novalidate>
+                            <div class="col-auto text-left form-col">
+                                <form class="mailchimp-form" novalidate>
                                     <p>Subcribe to our newsletter</p>
                                     <div class="form-group">
                                         <input id="email" placeholder="your e-mail" type="email" autocomplete="off">
