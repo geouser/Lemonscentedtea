cfaf46697b7aa2b7eb717be896fbafc0574c18a7
rusyi <olehrusy@gmail.com>
2018-05-01 09:56:59 +0300

    empty vimeo url

--- parts/flexible-content-blocks/block-video.php
+++ parts/flexible-content-blocks/block-video.php
@@ -1,21 +1,23 @@
 <?php 
-	if (get_field('youtube__vimeo')) {
+	if (get_sub_field('youtube__vimeo')) {
 		$video_url = get_sub_field('video_url');
 		$video_cover = get_sub_field('video_cover');
 		?>
 		<div class="video-block">
 			<a href="<?php echo $video_url; ?>" class="fancybox" style="background-image: url(<?php echo $video_cover['sizes']['figure']; ?>);"></a>
 		</div>
-	<?php } else { ?>
-		<div class="vimeo-container">
-			<div data-vimeo-url="<?php the_sub_field('vimeo_url'); ?>" data-vimeo-width="100vw" id="vimeo"></div>
-		</div>
+	<?php } else { 
+		if (get_sub_field('vimeo_url')) { ?>
+			<div class="vimeo-container">
+				<div data-vimeo-url="<?php the_sub_field('vimeo_url'); ?>" data-vimeo-width="100vw" id="vimeo"></div>
+			</div>
 
-		<script src="https://player.vimeo.com/api/player.js"></script>
-		<script>
-			var handstickPlayer = new Vimeo.Player('vimeo');
-		</script>
-	<?php }
+			<script src="https://player.vimeo.com/api/player.js"></script>
+			<script>
+				var handstickPlayer = new Vimeo.Player('vimeo');
+			</script>
+		<?php }
+	}
 ?>
 
 
