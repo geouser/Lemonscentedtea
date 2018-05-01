10a6046bf03d9c745cd250c34f3fd343e61ecfa6
rusyi <olehrusy@gmail.com>
2018-05-01 16:32:38 +0300

    home tablet

--- index.php
+++ index.php
@@ -20,24 +20,37 @@ get_header();
 	<section>
 		<div class="hero-slider"> <?
 		while ( have_rows('slider') ) : the_row(); 
+		
 			$link = '';
 			if (get_sub_field('custom_link')) {
 				$link = get_sub_field('custom_url');
 			} else {
 				$link = get_sub_field('link');
 			}
-			?>
-			<a href="<?php echo $link; ?>" class="slide item" style="background-image: url(<?php echo the_sub_field('image')?>); ">
-    			<div class="container">
-	    			<div class="slider-description">
-	                    <div class="slider-title"><?php the_sub_field('title'); ?></div>
-	                    <div class="slider-subtitle">
-							<?php the_sub_field('subtitle'); ?>
-	                    </div>
-	                </div>	
-    			</div>
-            </a>
-		<?
+
+			if (get_sub_field('slide_link')) {?>
+				<a href="<?php echo $link; ?>" class="slide item" style="background-image: url(<?php echo the_sub_field('image')?>); ">
+					<div class="container">
+						<div class="slider-description">
+							<div class="slider-title"><?php the_sub_field('title'); ?></div>
+							<div class="slider-subtitle">
+								<?php the_sub_field('subtitle'); ?>
+							</div>
+						</div>	
+					</div>
+				</a>
+			<?php } else { ?>
+				<div class="slide item" style="background-image: url(<?php echo the_sub_field('image')?>); ">
+					<div class="container">
+						<div class="slider-description">
+							<div class="slider-title"><?php the_sub_field('title'); ?></div>
+							<div class="slider-subtitle">
+								<?php the_sub_field('subtitle'); ?>
+							</div>
+						</div>	
+					</div>
+				</div>
+			<?php }
 		endwhile; ?>
 		</div> 
 	</section><?
